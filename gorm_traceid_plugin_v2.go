4d8371fe306e17f51823979c1b5c479de3d73e3b
张俊超 <zhangjunchao@xiaohulu.com>
2021-04-01 18:42:28 +0800

    1.Update gorm V2版本 调整慢日志阈值

--- gorm_traceid_plugin_v2.go
+++ gorm_traceid_plugin_v2.go
@@ -12,7 +12,7 @@ const (
 	callBackBeforeName = "core:before"
 	callBackAfterName  = "core:after"
 	startTime          = "_start_time"
-	SlowSqlTime        = 0
+	SlowSqlTime        = 0.5
 )
 
 type TracePlugin struct{}
@@ -78,7 +78,7 @@ func after(db *gorm.DB) {
 	sqlInfo.CostSeconds = time.Since(ts).Seconds()
 	sqlInfo.Table = db.Statement.Table
 
-	if time.Duration(sqlInfo.CostSeconds) >= SlowSqlTime {
+	if sqlInfo.CostSeconds >= SlowSqlTime {
 		wbuff := buffer.Buffer{}
 		wbuff.AppendString(sql)
 		wbuff.AppendString("-----执行时间：【 ")
