5b208940abce04cf225bda4c0781e0c135fc4ac3
张俊超 <zhangjunchao@xiaohulu.com>
2021-04-14 19:35:59 +0800

    1.Fixed gorm V2版本 sqlType类型初始化问题

--- gorm_V2/sql_type.go
+++ gorm_V2/sql_type.go
@@ -3,7 +3,7 @@ package gorm_V2
 type SqlInfo map[string][]interface{}
 
 func (i SqlInfo) Set(key string, val interface{}) {
-	i[key][0] = val
+	i[key] = []interface{}{val}
 }
 
 func (i SqlInfo) Get(key string) interface{} {
